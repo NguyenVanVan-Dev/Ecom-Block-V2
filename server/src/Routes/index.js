7d44b5ec1917504427e453168fc1367c130d2ef6
Nguyễn Văn Vấn <nvvan.0901.developer@gmail.com>
2022-03-21 21:43:31 +0700

    Init project--- Server connect database, config controller,models,routes

---
+++ server/src/Routes/index.js
@@ -0,0 +1,20 @@
+// const userRouter = require('./user');
+// const categoryRouter = require('./category');
+// const productRouter = require('./product');
+// const authRouter = require('./auth');
+const siteRouter = require('./site');
+// const contractRouter = require('./contract');
+
+
+function route(app){
+
+    // app.use('/admin', authRouter);
+    // app.use('/product', productRouter);
+    // app.use('/category', categoryRouter);
+    // app.use('/contract', contractRouter);
+    // app.use('/user', userRouter);
+    app.use('/', siteRouter);
+    
+}
+
+module.exports = route
